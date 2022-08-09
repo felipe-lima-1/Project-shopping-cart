0df08f487d5fd98872baed2b512653d884b36d33
Felipe <luizbrutante@gmail.com>
2022-08-09 19:32:57 -0300

    Requisitos 4 e 5

--- script.js
+++ script.js
@@ -12,6 +12,28 @@ const createCustomElement = (element, className, innerText) => {
   return e;
 };
 
+const getSkuFromProductItem = (item) => item.querySelector('span.item__sku').innerText;
+
+const cartItemClickListener = (event) => event.target.remove();
+
+const createCartItemElement = ({ sku, name, salePrice }) => {
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  li.addEventListener('click', cartItemClickListener);
+  return li;
+};
+
+const catchId = (item) => item.querySelector('span.item__sku').innerText;
+const random = async (element) => {
+  const event = await catchId(element.target.parentElement);
+  const result = await fetchItem(event);
+  const destruct = { sku: result.id, name: result.title, salePrice: result.price };
+  const card = createCartItemElement(destruct);
+  const felipe = document.querySelector('.cart__items');
+  felipe.appendChild(card);
+};
+
 const createProductItemElement = ({ sku, name, image }) => {
   const section = document.createElement('section');
   section.className = 'item';
@@ -26,18 +48,6 @@ const createProductItemElement = ({ sku, name, image }) => {
   return section;
 };
 
-const getSkuFromProductItem = (item) => item.querySelector('span.item__sku').innerText;
-
-const cartItemClickListener = (event) => event.target.remove();
-
-const createCartItemElement = ({ sku, name, salePrice }) => {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
-};
-
 const products = async () => {
   const object = await fetchProducts('computador');
   const itens = document.querySelector('.items');
@@ -48,16 +58,6 @@ const products = async () => {
     itens.appendChild(produtos);
   });
 };
-const catchId = (item) => item.querySelector('span.item__sku').innerText;
-const random = async (element) => {
-  const event = await catchId(element.target.parentElement);
-  const result = await fetchItem(event);
-  const destruct = { sku: result.id, name: result.title, salePrice: result.price };
-  const card = createCartItemElement(destruct);
-  const felipe = document.querySelector('.cart__items');
-  felipe.appendChild(card);
-};
-
 window.onload = () => {
   products();
 };
